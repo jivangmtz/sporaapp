0147fb1abb0519d8f079a4bfbe46114d823f0f7a
José Iván García Martínez <jgarciam@ia.com.mx>
2022-08-09 22:03:54 -0500

    Fix: add realm dependencies

---
+++ app/src/main/java/com/ivan/garcia/sporaapplication/Task.kt
@@ -0,0 +1,9 @@
+package com.ivan.garcia.sporaapplication
+
+import io.realm.RealmObject
+//import io.realm.kotlin.types.RealmObject
+
+class Task : RealmObject {
+    var name: String = "new task"
+    var status: String = "Open"
+}
