988e9cd41b0924439f1332e4b9a2b1e7d80ae18b
José Iván García Martínez <jgarciam@ia.com.mx>
2022-09-08 16:16:46 -0500

    Feat: add strings examples

--- app/src/main/java/com/ivan/garcia/sporaapplication/MainActivity.kt
+++ app/src/main/java/com/ivan/garcia/sporaapplication/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.ivan.garcia.sporaapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.text.Html
+import android.text.Spanned
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.HtmlCompat.FROM_HTML_MODE_LEGACY
 import com.ivan.garcia.sporaapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -15,5 +21,24 @@ class MainActivity : AppCompatActivity() {
         binding.imageView4.setImageResource(R.drawable.android_studio)
         binding.imageView5.setImageResource(R.drawable.android_studio_svg)
         binding.imageView6.setImageResource(R.drawable.weather_icon)
+
+        binding.textView.text = getText(R.string.app_name)
+        binding.textView.setText(R.string.app_name)
+
+        val helloStr: String = getString(R.string.hello)
+        println("the value of the string hello is: $helloStr")
+        binding.textView6.text = helloStr;
+
+        val name = "Mario"
+        val age = 20
+        val amount = 20.33333
+        binding.textView7.text = getString(R.string.string_format_ex, name, age, amount, true)
+
+        val text: String = getString(R.string.format_checkbox_tyc)
+        val styledText: Spanned = Html.fromHtml(text, FROM_HTML_MODE_LEGACY)
+        binding.textView8.text = styledText
+
+        val persons = resources.getStringArray(R.array.persons)
+        println(persons.contentToString())
     }
 }
