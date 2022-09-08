8b898f2b01af86a761ed9d7b965ecd5f054f571e
José Iván García Martínez <jgarciam@ia.com.mx>
2022-09-08 12:50:53 -0500

    Feat: set image resource in code

--- app/src/main/java/com/ivan/garcia/sporaapplication/MainActivity.kt
+++ app/src/main/java/com/ivan/garcia/sporaapplication/MainActivity.kt
@@ -2,10 +2,18 @@ package com.ivan.garcia.sporaapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ivan.garcia.sporaapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.imageView4.setImageResource(R.drawable.android_studio)
+        binding.imageView5.setImageResource(R.drawable.android_studio_svg)
+        binding.imageView6.setImageResource(R.drawable.weather_icon)
     }
 }
